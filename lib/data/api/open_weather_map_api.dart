3b34d1b4dcf3e4114950d698104a4940ef8fd16f
deadmist666 <kosenkovladysl4v@gmail.com>
2023-03-16 22:44:29 +0200

    Created OpenWeatherMap api service for fetching weather data

---
+++ lib/data/api/open_weather_map_api.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class OpenWeatherMapApi {
+  static const apiKey = '75a63f7421744b5d5f76aca3a94a6ae3';
+
+  String weatherDataUrlByLatLong(String latitude, String longitude) {
+    return 'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey';
+  }
+
+  String weatherDataUrlByCityName(String cityName) {
+    return 'https://api.openweathermap.org/data/2.5/weather?q=$cityName&appid=$apiKey';
+  }
+
+  Future<dynamic> fetchWeatherDataByLatLong(
+      String latitude, String longitude) async {
+    final response =
+        await http.get(Uri.parse(weatherDataUrlByLatLong(latitude, longitude)));
+    return jsonDecode(response.body);
+  }
+
+  Future<dynamic> fetchWeatherDataByCityName(String cityName) async {
+    final response =
+        await http.get(Uri.parse(weatherDataUrlByCityName(cityName)));
+    return jsonDecode(response.body);
+  }
+}
