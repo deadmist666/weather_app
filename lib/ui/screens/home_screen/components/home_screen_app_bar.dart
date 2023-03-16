5b21147c2a4a2e4ef0c72f63ba77b75960a4b019
deadmist666 <kosenkovladysl4v@gmail.com>
2023-03-16 20:02:25 +0200

    Created sample for the search term

--- lib/ui/screens/home_screen/components/home_screen_app_bar.dart
+++ lib/ui/screens/home_screen/components/home_screen_app_bar.dart
@@ -1,16 +1,38 @@
 import 'package:flutter/material.dart';
 
+import 'package:weather_app/ui/components/digressions.dart';
+import 'package:weather_app/ui/utils/color_palette.dart';
+
 class HomeScreenAppBar extends StatelessWidget implements PreferredSizeWidget {
   const HomeScreenAppBar({Key? key}) : super(key: key);
 
   @override
-  Size get preferredSize => Size(double.infinity, 150);
+  Size get preferredSize => Size(double.infinity, 100);
 
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      backgroundColor: Colors.black87,
-      elevation: 0,
+    final size = MediaQuery.of(context).size;
+
+    return Column(
+      children: [
+        SizedBox(height: size.height / 10),
+        Padding(
+          padding: Digression.paddingHorizontal24,
+          child: TextField(
+            textInputAction: TextInputAction.search,
+            decoration: InputDecoration(
+              contentPadding: Digression.paddingHorizontal24,
+              filled: true,
+              fillColor: ColorPalette.kPrimaryColor,
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide.none),
+              suffixIcon: Icon(Icons.search_outlined),
+              hintText: 'Search Location',
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
