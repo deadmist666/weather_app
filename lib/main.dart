f8a38adb5d2d356bf97b52bd4c52bb738fa9b712
deadmist666 <kosenkovladysl4v@gmail.com>
2023-03-16 22:45:16 +0200

    Added permission request for getting user location

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,25 @@
 import 'package:flutter/material.dart';
+
+import 'package:permission_handler/permission_handler.dart';
 import 'package:weather_app/ui/screens/splash_screen/splash_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Permission.location.request();
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-      ),
+      theme: ThemeData(),
       home: const SplashScreen(),
     );
   }
