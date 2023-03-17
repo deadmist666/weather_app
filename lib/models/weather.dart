46f905af3a842aeadf0ff4e5b1557b15140d1bb8
deadmist666 <kosenkovladysl4v@gmail.com>
2023-03-17 21:08:06 +0200

    Created a Weather factory constructor fromJson for fetching data and implementing it in the Weather object.

--- lib/models/weather.dart
+++ lib/models/weather.dart
@@ -1,12 +1,21 @@
 class Weather {
-  final int temp;
+  final double temp;
   final String weatherImage;
   final String cityName;
 
-  Weather({required this.weatherImage, required this.temp, required this.cityName});
+  Weather(
+      {required this.weatherImage, required this.temp, required this.cityName});
+
+  factory Weather.fromJson(Map<String, dynamic> json) {
+    return Weather(
+      temp: json['main']['temp'],
+      weatherImage: json['weather'][0]['icon'],
+      cityName: json['name'],
+    );
+  }
 }
 
 List<Weather> fooWeatherList = [
   Weather(weatherImage: 'sun_img', temp: 24, cityName: 'Zhytomyr'),
-  Weather(weatherImage: 'cloudy_img',temp: 10, cityName: 'Kaniv'),
+  Weather(weatherImage: 'cloudy_img', temp: 10, cityName: 'Kaniv'),
 ];
