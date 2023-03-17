779da829186442379c590e517601bf04a9a66430
deadmist666 <kosenkovladysl4v@gmail.com>
2023-03-17 21:09:26 +0200

    Changed type of lat and long to double

--- lib/data/api/open_weather_map_api.dart
+++ lib/data/api/open_weather_map_api.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart' as http;
 class OpenWeatherMapApi {
   static const apiKey = '75a63f7421744b5d5f76aca3a94a6ae3';
 
-  String weatherDataUrlByLatLong(String latitude, String longitude) {
+  String weatherDataUrlByLatLong(double latitude, double longitude) {
     return 'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey';
   }
 
@@ -14,7 +14,7 @@ class OpenWeatherMapApi {
   }
 
   Future<dynamic> fetchWeatherDataByLatLong(
-      String latitude, String longitude) async {
+      {double latitude = 0.0, double longitude = 0.0}) async {
     final response =
         await http.get(Uri.parse(weatherDataUrlByLatLong(latitude, longitude)));
     return jsonDecode(response.body);
