b1d25977f723ed57167e190f95201b1fa29d1782
deadmist666 <kosenkovladysl4v@gmail.com>
2023-03-17 21:22:35 +0200

    Changed the return type to the Weather object Changed type of lat and long to double

--- lib/data/repository/open_weather_map_repository.dart
+++ lib/data/repository/open_weather_map_repository.dart
@@ -1,9 +1,14 @@
 import 'package:weather_app/data/api/open_weather_map_api.dart';
+import 'package:weather_app/models/weather.dart';
 
 class OpenWeatherMapRepository {
-  Future<dynamic> fetchWeatherDataByLatLong(String latitude, String longitude) async{
-    return OpenWeatherMapApi().fetchWeatherDataByLatLong(latitude, longitude);
+  Future<dynamic> fetchWeatherDataByLatLong(
+      {double latitude = 0.0, double longitude = 0.0}) async {
+    final weatherData = await OpenWeatherMapApi()
+        .fetchWeatherDataByLatLong(latitude: latitude, longitude: longitude);
+    return Weather.fromJson(weatherData);
   }
+
   Future<dynamic> fetchWeatherDataByCityName(String cityName) async {
     return OpenWeatherMapApi().fetchWeatherDataByCityName(cityName);
   }
