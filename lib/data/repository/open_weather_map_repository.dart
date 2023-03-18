0a233c034a84d5a908b3856f1f1c148b81fc364d
deadmist666 <kosenkovladysl4v@gmail.com>
2023-03-18 22:13:17 +0200

    Made strong typing

--- lib/data/repository/open_weather_map_repository.dart
+++ lib/data/repository/open_weather_map_repository.dart
@@ -2,7 +2,7 @@ import 'package:weather_app/data/api/open_weather_map_api.dart';
 import 'package:weather_app/models/weather.dart';
 
 class OpenWeatherMapRepository {
-  Future<dynamic> fetchWeatherDataByLatLong(
+  Future<Weather> fetchWeatherDataByLatLong(
       {double latitude = 0.0, double longitude = 0.0}) async {
     final weatherData = await OpenWeatherMapApi()
         .fetchWeatherDataByLatLong(latitude: latitude, longitude: longitude);
