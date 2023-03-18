d153df7a5ba081d9b992c03c03e4fec95a139529
deadmist666 <kosenkovladysl4v@gmail.com>
2023-03-18 22:24:24 +0200

    Implemented Weather bloc on the page

--- lib/ui/screens/home_screen/components/home_screen_weather_info_column.dart
+++ lib/ui/screens/home_screen/components/home_screen_weather_info_column.dart
@@ -1,22 +1,47 @@
 import 'package:flutter/material.dart';
 
-import 'package:weather_app/models/weather.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/bloc/weather_bloc/weather_bloc.dart';
+import 'package:weather_app/models/location.dart';
 
-class HomeScreenWeatherInfoColumn extends StatelessWidget {
-  final Weather weatherInfo;
+class HomeScreenWeatherInfoColumn extends StatefulWidget {
+  final LocationData locationData;
 
-  const HomeScreenWeatherInfoColumn({Key? key, required this.weatherInfo})
+  const HomeScreenWeatherInfoColumn({Key? key, required this.locationData})
       : super(key: key);
 
   @override
+  State<HomeScreenWeatherInfoColumn> createState() => _HomeScreenWeatherInfoColumnState();
+}
+
+class _HomeScreenWeatherInfoColumnState extends State<HomeScreenWeatherInfoColumn> {
+  late WeatherBloc _weatherBloc;
+
+  @override
+  void initState() {
+    super.initState();
+    _weatherBloc = context.read<WeatherBloc>();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        Text(weatherInfo.weatherImage),
-        Text(weatherInfo.cityName),
-        Text('${weatherInfo.temp}'),
-      ],
-    );
+    return BlocBuilder<WeatherBloc, WeatherState>(
+        bloc: _weatherBloc,
+        builder: (context, state) {
+          if (state is WeatherInitialState) {
+            _weatherBloc.add(WeatherFetchedEvent(widget.locationData));
+          } else if(state is WeatherLoadingState){
+            return CircularProgressIndicator();
+          } else if(state is WeatherLoadedState){
+            final weatherData = state.weatherData;
+            return Column(
+              children: [
+                Text(weatherData.cityName),
+              ],
+            );
+          }
+          return Text('Error');
+        }
+        );
   }
 }
